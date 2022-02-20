d132995857e7ecf043fee96f2b5e074b1d366136
Vanderson Rodrigues <vanderson@tutamail.com>
2022-02-20 13:21:16 -0300

    [fish] Remove bass plugin

--- fish/.config/fish/functions/nvm.fish
+++ fish/.config/fish/functions/nvm.fish
@@ -1,5 +1,3 @@
-# bass is a plugin to use bash functionalities
-# https://github.com/edc/bass
 function nvm
-    bass source ~/.nvm/nvm.sh --no-use ';' nvm $argv
+    bash -c 'source ~/.nvm/nvm.sh --no-use ";" nvm $argv'
 end
