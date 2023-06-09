7cff7010720eaf4d08ab1ea4861877b5b1ef92f2
Vanderson Rodrigues <vanderson@tutamail.com>
2023-06-09 10:18:02 -0300

    [emacs] Update custom.el

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -6,7 +6,7 @@
  '(custom-safe-themes
    '("4c56af497ddf0e30f65a7232a8ee21b3d62a8c332c6b268c81e9ea99b11da0d3" "fee7287586b17efbfda432f05539b58e86e059e78006ce9237b8732fde991b4c" "51ec7bfa54adf5fff5d466248ea6431097f5a18224788d0bd7eb1257a4f7b773" default))
  '(package-selected-packages
-   '(dired-subtree all-the-icons-dired all-the-icons diminish minions which-key vimrc-mode use-package-ensure-system-package undo-tree typescript-mode try solarized-theme rust-mode rainbow-delimiters org-modern moody mixed-pitch magit gcmh evil-surround evil-collection eglot dired-ranger command-log-mode clojure-mode-extra-font-locking cider auto-compile)))
+   '(## markdown-mode dired-subtree all-the-icons-dired all-the-icons diminish minions which-key vimrc-mode use-package-ensure-system-package undo-tree typescript-mode try solarized-theme rust-mode rainbow-delimiters org-modern moody mixed-pitch magit gcmh evil-surround evil-collection eglot dired-ranger command-log-mode clojure-mode-extra-font-locking cider auto-compile)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
